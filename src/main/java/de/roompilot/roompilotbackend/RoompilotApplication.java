642238c423c0765b4e8a8768bad825d67ccb254e
Luca A. Schumacher <l.schumacher@luftiger.eu>
2023-03-17 08:24:24 +0100

    Initial commit

---
+++ src/main/java/de/roompilot/roompilotbackend/RoompilotApplication.java
@@ -0,0 +1,13 @@
+package de.roompilot.roompilotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoompilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoompilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/roompilot/roompilotbackend/RoompilotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.roompilot.roompilotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoompilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
